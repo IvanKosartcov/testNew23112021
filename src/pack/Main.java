b3660691d82eef1c586acd5e72a85d94d44aa899
Ivan_ItStep <ivan.kosar@yandex.ru>
2021-11-24 00:12:05 +0500

    tool Files

--- src/pack/Main.java
+++ src/pack/Main.java
@@ -1,8 +1,25 @@
 package pack;
 
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("start");
+        Path path = Paths.get("file.txt");
+        try {
+            Files.write(path, "Привет NIO".getBytes("cp1251"));
+            byte[] bytes = Files.readAllBytes(path);
+            System.out.println(new String(bytes, "cp1251"));
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
